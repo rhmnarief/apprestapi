d3fac6ead6d6b90068a5da5a64d7c09d2daffe08
Rahman Arief <rhmnarief8@gmail.com>
2021-10-02 21:05:24 +0700

    post data berhasil

--- controller.js
+++ controller.js
@@ -3,33 +3,52 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
-    response.ok("Aplikasi REST API berjalan !",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API berjalan !", res)
 };
 
 // menampilkan semua data mahasiswa
 
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error, rows , fields){
-        if(error){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
             connection.log(error);
-        }else{
-            response.ok(rows ,res)
+        } else {
+            response.ok(rows, res)
         }
     });
 };
 
 // menampilkan data mahasiswa berdasarkan id
 
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
-    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id] , 
-        function(error, rows, fields){
-            if(error){
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
                 connection.log(error);
-            }else{
-                response.ok(rows , res)
+            } else {
+                response.ok(rows, res)
             }
         }
     );
+};
+// menambah data mahasiswa
+
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)',
+        [nim, nama, jurusan],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("berhasil menambahkan data", res)
+            }
+        }
+    );
+
 };
