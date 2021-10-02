aae41ea54c2dae3ac39c7f6c35fff30abfe2a975
Rahman Arief <rhmnarief8@gmail.com>
2021-10-02 21:19:21 +0700

    berhasil hapus data

--- controller.js
+++ controller.js
@@ -71,3 +71,17 @@ exports.ubahMahasiswa = function (req, res) {
         }
     );
 };
+
+// menghapus data mahasiswa
+exports.hapusMahasiswa = function (req, res) {
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("berhasil hapus data", res)
+            }
+        }
+    );
+};
--- routes.js
+++ routes.js
@@ -5,16 +5,14 @@ module.exports = function(app){
     
     app.route('/')
         .get(jsonku.index);
-    
     app.route('/tampil')
         .get(jsonku.tampilsemuamahasiswa);
-
      app.route('/tampil/:id')
-        .get(jsonku.tampilberdasarkanid);
-
+         .get(jsonku.tampilberdasarkanid);
     app.route('/tambah')
         .post(jsonku.tambahMahasiswa);
-
     app.route('/ubah')
         .put(jsonku.ubahMahasiswa);
+    app.route('/hapus')
+        .delete(jsonku.hapusMahasiswa);
 }
